608c78991ed122e5e6a3c77d0ce3931533780608
unknown <mathewpqmiller@gmail.com>
2021-03-01 12:25:26 -0800

    Commit employee_queries_sql file. First Data Analysis query successfully completed

---
+++ employee_queries.sql
@@ -0,0 +1,13 @@
+-- EMPLOYEE QUERIES for the SQL Homework - Employee Database: A Mystery in Two Parts
+
+-- 1) List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, 
+		employees.last_name, 
+		employees.first_name, 
+		employees.gender, 
+		salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no
+;
+
