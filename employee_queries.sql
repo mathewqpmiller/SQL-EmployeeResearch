29287a6e488f76d86af3da6764894ab73b65498e
unknown <mathewpqmiller@gmail.com>
2021-03-01 13:56:39 -0800

    Employee_queries data analysis question 8 completed successfully

--- employee_queries.sql
+++ employee_queries.sql
@@ -91,3 +91,13 @@ WHERE dept_emp.dept_no IN
 		OR dept_name = 'Development'
 	)
 ;
+
+-- 8) In descending order, list the frequency count of employee last names, i.e., how many employees 
+--    share each last name.
+SELECT 
+last_name, 
+count(last_name) AS "last_name_count"
+FROM employees
+GROUP BY last_name
+ORDER BY last_name_count DESC
+;
