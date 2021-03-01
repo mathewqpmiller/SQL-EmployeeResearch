32631c7a4d0a44528139f0a432773fc95f42d845
unknown <mathewpqmiller@gmail.com>
2021-03-01 13:02:55 -0800

    Employee_queries data analysis question 4 completed successfully

--- employee_queries.sql
+++ employee_queries.sql
@@ -34,3 +34,15 @@ FROM dept_manager
 LEFT JOIN departments ON dept_manager.dept_no = departments.dept_no
 LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no
 ;
+
+-- 4) List the department of each employee with the following information: employee number, last name, first name,
+--	  and department name.
+SELECT
+employees.emp_no, 
+employees.last_name, 
+employees.first_name, 
+departments.dept_name
+FROM employees
+LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+LEFT JOIN departments ON departments.dept_no = dept_emp.dept_no
+;
