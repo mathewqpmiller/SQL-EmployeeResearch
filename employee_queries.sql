20e133fb21bca71cae9c994a290adbd5228ee40b
unknown <mathewpqmiller@gmail.com>
2021-03-01 12:43:11 -0800

    Data Analysis question 2 queries successfully

--- employee_queries.sql
+++ employee_queries.sql
@@ -11,3 +11,11 @@ JOIN salaries
 ON employees.emp_no = salaries.emp_no
 ;
 
+-- 2) List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name,
+		employees.last_name,
+		employees.hire_date
+FROM employees
+WHERE hire_date
+BETWEEN '1986-01-01' AND '1986-12-31'
+;
