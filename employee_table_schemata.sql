78634458a846e5903e8fe1bac90e935ea8b4b769
unknown <mathewpqmiller@gmail.com>
2021-03-01 10:29:00 -0800

    Employees.csv table schemata and import successfully completed

---
+++ employee_table_schemata.sql
@@ -0,0 +1,25 @@
+-- My Table Schemata for the SQL Homework - Employee Database: A Mystery in Two Parts
+
+-- Drop any tables if they exist
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- Because of primary keys and foreign keys I recommend that the tables be built in this order; 
+
+-- Create 'employees' table and execute
+CREATE TABLE employees (
+    emp_no INT NOT NULL PRIMARY KEY,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR (20) NOT NULL,
+    last_name VARCHAR (20) NOT NULL,
+    gender VARCHAR (10) NOT NULL,
+    hire_date DATE NOT NULL
+);
+
+-- Import successfully completed. Import returned 300024 rows of data; limit view to 15.
+-- I had to delete one column from the employees.csv for import to successfully complete.
+SELECT * FROM employees LIMIT 15;
